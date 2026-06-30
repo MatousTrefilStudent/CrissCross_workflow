cea7954d059672c82ee95c18919298366ad5fcb4
MatousTrefilStudent <matous.trefil@student.mgvsetin.cz>
2026-06-30 15:54:59 +0200

    testing github actions

--- modules/player.py
+++ modules/player.py
@@ -15,3 +15,6 @@ class Player:
         if result is not True:
             raise ValueError("board.make_move() did not return True (you can´t place your symbol there)")
         return result
+
+    def test(self):
+        print("something")
